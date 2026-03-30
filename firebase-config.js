3bcc6b6d92cb07c242bf1884b40186fb794f6c7e
root <root@vmi3189039.contaboserver.net>
2026-03-30 08:12:55 +0200

    fix: publish firebase config for github pages

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDgbgEvJ7r6P9dRo715MBi24t_QWSZosyY",
+  authDomain: "study-time-dashboard.firebaseapp.com",
+  projectId: "study-time-dashboard",
+  storageBucket: "study-time-dashboard.firebasestorage.app",
+  messagingSenderId: "254474802163",
+  appId: "1:254474802163:web:2e65917dc1eb1833feb886",
+  measurementId: "G-T3W63CCWGC"
+};
